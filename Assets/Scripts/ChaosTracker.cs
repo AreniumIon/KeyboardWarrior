14fd66aa6ccad5ed858ef9bd4c145ab534bf4d54
AreniumIon <jeffreydickenson9@gmail.com>
2022-02-08 20:15:18 -0600

    TMPro import, GameScene creation, script creation

---
+++ Assets/Scripts/ChaosTracker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChaosTracker : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _chaosPointsText;
+
+    int chaosPoints = 0;
+
+    public void GainChaosPoints(int points)
+    {
+        chaosPoints += points;
+        SetText();
+    }
+
+    private void SetText()
+    {
+        _chaosPointsText.text = "Chaos Points: " + chaosPoints.ToString();
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController i;
+
+    private void Awake()
+    {
+        if (i == null)
+        {
+            i = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
