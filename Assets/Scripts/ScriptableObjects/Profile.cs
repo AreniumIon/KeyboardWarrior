dee722c049d914964e212d6ec362dbe09a6c08ba
Unknown <team.telvy@gmail.com>
2022-02-16 19:01:02 -0600

    Working Random NPC Profiles
    Working Random NPC Profiles

---
+++ Assets/Scripts/ScriptableObjects/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New NPC Profile", menuName = "Profile")]
+public class Profile : ScriptableObject
+{
+    public Sprite[] _head;
+    public Sprite[] _body;
+    public Sprite[] _eyes;
+    public Sprite[] _mouth;
+    public Sprite[] _background;
+
+}
---
+++ Assets/Scripts/UI/ProfileDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileDisplay : MonoBehaviour
+{
+    //our base scriptable object reference
+    public Profile _npcProfile;
+
+    //profile body part image references
+    public Image _pfpHead;
+    public Image _pfpBody;
+    public Image _pfpEyes;
+    public Image _pfpMouth;
+    public Image _pfpBackground;
+
+    //random numbeer variables
+    private int randHead;
+    private int randBody;
+    private int randEyes;
+    private int randMouth;
+    private int randBackground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // AssignNewProfile();
+    }
+
+    private void AssignNewProfile()
+    {
+        randHead = Random.Range(0, _npcProfile._head.Length);
+        _pfpHead.sprite = _npcProfile._head[randHead];
+
+        randBody = Random.Range(0, _npcProfile._body.Length);
+        _pfpBody.sprite = _npcProfile._body[randBody];
+
+        randEyes = Random.Range(0, _npcProfile._eyes.Length);
+        _pfpEyes.sprite = _npcProfile._eyes[randEyes];
+
+        randMouth = Random.Range(0, _npcProfile._mouth.Length);
+        _pfpMouth.sprite = _npcProfile._mouth[randMouth];
+
+        randBackground = Random.Range(0, _npcProfile._background.Length);
+        _pfpBackground.sprite = _npcProfile._background[randBackground];
+
+    }
+
+}
