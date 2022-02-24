b0a363bc938e9741a6d73898bd2307a233fbffe3
Unknown <team.telvy@gmail.com>
2022-02-23 19:22:40 -0600

    InputUsernameController
    prefab that accepts the name of the user and displays it via text during gameplay

---
+++ Assets/Scripts/Macro/InputUsernameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputUsernameController : MonoBehaviour
+{
+    [SerializeField] public string _userName = "";
+    [SerializeField] TMP_InputField _inputField;
+    [SerializeField] GameObject _textDisplay = null;
+
+    public void StoreUsername()
+    {
+        if(string.IsNullOrEmpty(_inputField.text))
+        {
+            _userName = "User";
+            _textDisplay.GetComponent<TextMeshProUGUI>().text = "@" + _userName;
+        }
+        else
+        {
+            _userName = _inputField.text;
+            _textDisplay.GetComponent<TextMeshProUGUI>().text = "@" + _userName;
+        }
+    }
+
+}
