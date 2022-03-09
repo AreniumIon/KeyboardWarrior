1ecde934e128db0c6bbf6754ff1e4ea80aec333f
AreniumIon <jeffreydickenson9@gmail.com>
2022-03-09 16:40:59 -0600

    Conversation progress

---
+++ Assets/Scripts/UI/ConversationDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ConversationDisplay : MonoBehaviour
+{
+    public static float MESSAGE_TIME = .5f; // Time it takes for player's message to type out
+    public static float PAUSE_TIME = .5f; // Time between player's message and NPC typing
+    public static float RESPONSE_TIME = .5f; // Time NPC types for
+
+    [SerializeField] TextMeshProUGUI conversationText;
+
+    private void Start()
+    {
+        ResetDisplay();
+    }
+
+    public void ResetDisplay()
+    {
+        conversationText.text = "@" + GameController.i.playerController.Username + ":";
+    }
+
+    public void StartConversation()
+    {
+        StartCoroutine(DoConversation());
+    }
+
+    public IEnumerator DoConversation()
+    {
+        yield return null;
+    }
+}
