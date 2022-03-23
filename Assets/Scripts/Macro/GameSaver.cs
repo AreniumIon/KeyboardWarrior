4d8b03627131a5f33beecd06faf96eaccffbe0b6
AreniumIon <jeffreydickenson9@gmail.com>
2022-03-23 16:40:54 -0500

    SaveButton replaced with automatic saving OnApplicationQuit

---
+++ Assets/Scripts/Macro/GameSaver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSaver : MonoBehaviour
+{
+    // Have to store in separate script, because SaveController isn't a MonoBehavior, so it doesn't have access to OnApplicationQuit.
+    private void OnApplicationQuit()
+    {
+        SaveController.Save();
+    }
+}
