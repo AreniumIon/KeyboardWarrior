7abcdb09f05b945a43e64cc2932c2cc1f9423d15
AreniumIon <jeffreydickenson9@gmail.com>
2022-03-23 17:58:18 -0500

    Animated strike and account termination panels

---
+++ Assets/Scripts/Helpers/MathFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathFunctions
+{
+    public static float Square(float x) { return x * x; }
+    public static float Flip(float x) { return 1 - x; }
+
+    // 1D Scaling
+    public static float SmoothStart2(float t) { return Square(t); }
+    public static float SmoothStart3(float t) { return Square(t) * t; }
+    public static float SmoothStart4(float t) { return Square(Square(t)); }
+
+    public static float SmoothStop2(float t) { float flipT = Flip(t); return 1 - Square(flipT); }
+    public static float SmoothStop3(float t) { float flipT = Flip(t); return 1 - Square(flipT) * flipT; }
+    public static float SmoothStop4(float t) { float flipT = Flip(t); return 1 - Square(Square(flipT)); }
+}
---
+++ Assets/Scripts/UI/StrikeCanvasAnimator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrikeCanvasAnimator : MonoBehaviour
+{
+    [SerializeField] Transform _targetTransform;
+
+    private static Vector2 CENTER_POS = new Vector2(0f, 0f);
+    private static Vector2 START_POS = new Vector2(0f, 1500f);
+
+    private static float MOVE_TIME = 1f;
+
+    private void OnEnable()
+    {
+        SetToStartPos();
+        StartCoroutine(MoveToCenter());
+    }
+
+    private void SetToStartPos()
+    {
+        _targetTransform.localPosition = START_POS;
+    }
+
+    private IEnumerator MoveToCenter()
+    {
+        float timer = 0f;
+
+        while (timer < MOVE_TIME)
+        {
+            timer += Time.deltaTime;
+
+            float progress = MathFunctions.SmoothStop3(timer / MOVE_TIME);
+
+
+            float x = Mathf.Lerp(START_POS.x, CENTER_POS.x, progress);
+            float y = Mathf.Lerp(START_POS.y, CENTER_POS.y, progress);
+
+            _targetTransform.localPosition = new Vector2(x, y);
+
+            yield return null;
+        }
+    }
+}
