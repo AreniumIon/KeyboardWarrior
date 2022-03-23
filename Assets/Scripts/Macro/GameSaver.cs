8d0c26e947402f9cdadb468e27b2539b1ebc60fa
AreniumIon <jeffreydickenson9@gmail.com>
2022-03-23 16:55:03 -0500

    Backup auto-save mechanic

--- Assets/Scripts/Macro/GameSaver.cs
+++ Assets/Scripts/Macro/GameSaver.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class GameSaver : MonoBehaviour
 {
+    static float AUTO_SAVE_SECONDS = 10f;
+
     // Have to store in separate script, because SaveController isn't a MonoBehavior, so it doesn't have access to OnApplicationQuit.
     private void OnApplicationQuit()
     {
         SaveController.Save();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(AutoSave());
+    }
+
+    private IEnumerator AutoSave()
+    {
+        yield return new WaitForSeconds(AUTO_SAVE_SECONDS);
+        SaveController.Save();
+        StartCoroutine(AutoSave());
     }
 }
