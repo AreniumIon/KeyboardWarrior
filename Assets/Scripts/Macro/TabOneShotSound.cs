522e2c9ef869281a73670b9346e9238c27cc0443
Unknown <team.telvy@gmail.com>
2022-03-23 21:25:46 -0500

    Tab Sound Pool Prefabs
    Tab Sound Pool Prefabs

---
+++ Assets/Scripts/Macro/TabOneShotSound.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabOneShotSound : MonoBehaviour
+{
+    private AudioSource _audioSource = null;
+
+    [Header("Object Pooling")]
+    TabSoundPool _tabSoundPool = null;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    public void AssignTabSoundPool(TabSoundPool tabSoundPool)
+    {
+        _tabSoundPool = tabSoundPool;
+    }
+
+    public void OnEnable()
+    {
+        _audioSource.Play();
+        StartCoroutine(WaitToRemoveSelf());
+    }
+
+    private IEnumerator WaitToRemoveSelf()
+    {
+        //yield return new WaitForSeconds(_audioSource.clip.length);
+        yield return new WaitForSeconds(0.5f);
+        RemoveSelf();
+    }
+
+    private void RemoveSelf()
+    {
+        if (_tabSoundPool != null)
+        {
+            //return to Pool, instead of Destroy
+            _tabSoundPool.ReturnToPool(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/UI/TabController.cs
+++ Assets/Scripts/UI/TabController.cs
@@ -6,11 +6,18 @@ public class TabController : MonoBehaviour
 {
     [SerializeField] AudioClip tabSound;
 
+    [Header("Object Pooling")]
+    [SerializeField] TabSoundPool _tabSoundPool = null;
+
     public void ClickMessagesTab()
     {
         if (GameController.i.keyboardWarriorSM.CurrentState is MessagesState)
             return;
-        OneShotSoundController.PlayClip2D(tabSound, 1f);
+        //OneShotSoundController.PlayClip2D(tabSound, 1f);
+        TabOneShotSound newTabSound = _tabSoundPool.ActivateFromPool();
+        
+        newTabSound.AssignTabSoundPool(_tabSoundPool);
+        newTabSound.gameObject.SetActive(true);
         GameController.i.keyboardWarriorSM.ChangeState<MessagesState>();
     }
 
@@ -18,7 +25,13 @@ public class TabController : MonoBehaviour
     {
         if (GameController.i.keyboardWarriorSM.CurrentState is ShopState)
             return;
-        OneShotSoundController.PlayClip2D(tabSound, 1f);
+        //OneShotSoundController.PlayClip2D(tabSound, 1f);
+        TabOneShotSound newTabSound = _tabSoundPool.ActivateFromPool();
+        
+        newTabSound.AssignTabSoundPool(_tabSoundPool);
+        newTabSound.gameObject.SetActive(true);
         GameController.i.keyboardWarriorSM.ChangeState<ShopState>();
     }
+
+  
 }
